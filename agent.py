1150da3993eb4d7abe5f1395322a3553b22d9210
Grae <562696940@qq.com>
2023-04-26 16:51:24 +0800

    加注释

--- agent.py
+++ agent.py
@@ -22,25 +22,31 @@ class Agent:
 
 
     def get_state(self, game):
-        head = game.snake[0]
-        point_l = Point(head.x - 20, head.y)
-        point_r = Point(head.x + 20, head.y)
-        point_u = Point(head.x, head.y - 20)
-        point_d = Point(head.x, head.y + 20)
+        # 这是一个贪吃蛇游戏中获取当前状态的函数，
+        # 将当前贪吃蛇头部四周以及前方的空间状态和食物相对于贪吃蛇头部的位置状态，作为状态向量，用来作为强化学习的输入。
+        # 具体来说，该函数通过判断当前贪吃蛇头部四周的空间状态是否为危险（即是否会撞到障碍物），
+        # 以及当前贪吃蛇前进方向和当前贪吃蛇头部四周的空间状态是否有障碍物来得出三个状态。
+        # 接着，该函数用一个4维向量表示当前贪吃蛇的运动方向，一个4维向量表示食物在相对于贪吃蛇头部的位置，
+        # 将这些信息合并为一个长度为11的向量返回。
+        head = game.snake[0] # 贪吃蛇头部
+        point_l = Point(head.x - 20, head.y) # 贪吃蛇头部左边的点（20）
+        point_r = Point(head.x + 20, head.y) # 贪吃蛇头部右边的点（20）
+        point_u = Point(head.x, head.y - 20) # 贪吃蛇头部上边的点（20）
+        point_d = Point(head.x, head.y + 20) # 贪吃蛇头部下边的点（20）
         
-        dir_l = game.direction == Direction.LEFT
-        dir_r = game.direction == Direction.RIGHT
-        dir_u = game.direction == Direction.UP
-        dir_d = game.direction == Direction.DOWN
+        dir_l = game.direction == Direction.LEFT  # 这行代码是在检查当前蛇的移动方向是否是左边，返回的是一个bool类型的值。如果蛇的移动方向是左边，dir_l的值就是True，否则为False。
+        dir_r = game.direction == Direction.RIGHT # 
+        dir_u = game.direction == Direction.UP    
+        dir_d = game.direction == Direction.DOWN  
 
         state = [
-            # Danger straight
-            (dir_r and game.is_collision(point_r)) or 
+            # Danger straight，前方危险
+            (dir_r and game.is_collision(point_r)) or # 如果蛇的移动方向是右边，且右边有障碍物，那么dir_r的值就是True，否则为False。如果dir_r的值是True，那么game.is_collision(point_r)的值就是True，否则为False。如果dir_r和game.is_collision(point_r)的值都是True，那么(dir_r and game.is_collision(point_r))的值就是True，否则为False。
             (dir_l and game.is_collision(point_l)) or 
             (dir_u and game.is_collision(point_u)) or 
             (dir_d and game.is_collision(point_d)),
 
-            # Danger right
+            # Danger right，
             (dir_u and game.is_collision(point_r)) or 
             (dir_d and game.is_collision(point_l)) or 
             (dir_l and game.is_collision(point_u)) or 
@@ -52,7 +58,7 @@ class Agent:
             (dir_r and game.is_collision(point_u)) or 
             (dir_l and game.is_collision(point_d)),
             
-            # Move direction
+            # Move direction 移动方向
             dir_l,
             dir_r,
             dir_u,
@@ -67,7 +73,7 @@ class Agent:
 
         return np.array(state, dtype=int)
 
-    def remember(self, state, action, reward, next_state, done):
+    def remember(self, state, action, reward, next_state, done): #储存状态，动作，奖励，下一个状态，是否结束
         self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
 
     def train_long_memory(self):
@@ -81,10 +87,11 @@ class Agent:
         #for state, action, reward, nexrt_state, done in mini_sample:
         #    self.trainer.train_step(state, action, reward, next_state, done)
 
+    # 迭代一次，训练一次
     def train_short_memory(self, state, action, reward, next_state, done):
         self.trainer.train_step(state, action, reward, next_state, done)
 
-    def get_action(self, state):
+    def get_action(self, state): # 行为选择函数
         # random moves: tradeoff exploration / exploitation
         self.epsilon = 80 - self.n_games
         final_move = [0,0,0]
@@ -108,31 +115,31 @@ def train():
     agent = Agent()
     game = SnakeGameAI()
     while True:
-        # get old state
-        state_old = agent.get_state(game)
+        # get old state，这里的state是一个list，包含了11个元素，分别是
+        state_old = agent.get_state(game)  # game返回的是一个list，包含了3个元素，分别是蛇的位置，食物的位置，蛇的移动方向
 
-        # get move
+        # get move, 根据state_old，选择一个动作
         final_move = agent.get_action(state_old)
 
-        # perform move and get new state
+        # perform move and get new state, 环境接受动作，返回新的状态，奖励，是否结束，得分
         reward, done, score = game.play_step(final_move)
         state_new = agent.get_state(game)
 
-        # train short memory
+        # train short memory，训练短期记忆，走一步训练一步
         agent.train_short_memory(state_old, final_move, reward, state_new, done)
 
-        # remember
+        # remember，将状态，动作，奖励，下一个状态，是否结束存储到memory中
         agent.remember(state_old, final_move, reward, state_new, done)
 
         if done:
             # train long memory, plot result
             game.reset()
             agent.n_games += 1
-            agent.train_long_memory()
+            agent.train_long_memory() # 长期记忆，走完一局训练一次
 
-            if score > record:
+            if score > record: # 记录最高分
                 record = score
-                agent.model.save()
+                agent.model.save() # 保存模型
 
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
@@ -140,7 +147,7 @@ def train():
             total_score += score
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
-            plot(plot_scores, plot_mean_scores)
+            plot(plot_scores, plot_mean_scores) # 画图,每一局的得分和平均得分
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -4,7 +4,19 @@ import torch.optim as optim
 import torch.nn.functional as F
 import os
 
-class Linear_QNet(nn.Module):
+class Linear_QNet(nn.Module): 
+    # Qnet通常指的是“Q网络”（Q-network），
+    # 是一种基于神经网络的强化学习算法。
+    # 它可以用来解决基于状态-行动值函数（state-action value function）的强化学习问题，
+    # 其中策略和价值函数通过学习和迭代来优化。
+
+    # Q网络的主要思想是使用神经网络来逼近状态-行动值函数，即Q值。
+    # 通过在神经网络中输入状态，输出每个可能行动的Q值，
+    # 然后选择最大Q值对应的行动来更新策略和价值函数。
+    # Q网络通常与深度学习结合使用，
+    # 就是根据当前状态来预测下一步的行动
+
+    
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
@@ -24,7 +36,7 @@ class Linear_QNet(nn.Module):
         torch.save(self.state_dict(), file_name)
 
 
-class QTrainer:
+class QTrainer: # Q学习器
     def __init__(self, model, lr, gamma):
         self.lr = lr
         self.gamma = gamma
@@ -48,19 +60,25 @@ class QTrainer:
             done = (done, )
 
         # 1: predicted Q values with current state
-        pred = self.model(state)
+        pred = self.model(state) # 通过Q网络得到预测的各个动作的价值
 
-        target = pred.clone()
-        for idx in range(len(done)):
-            Q_new = reward[idx]
-            if not done[idx]:
+        target = pred.clone() # 克隆预测动作的价值
+        
+        for idx in range(len(done)): # len(done) 表示游戏进行了多少步
+            Q_new = reward[idx]      # 每一步的得到的奖励为Q_new
+            if not done[idx]:        # 如果游戏没有结束
                 Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
-
+                # Q_new = r + y * max(next_predicted Q value)
+                # Q_new = 该步的奖励 + 折扣因子 * 下一步的状态的各个动作的最大价值
+                # 使用当前状态和动作获得的奖励作为监督信号
+                # 当前动作获得的奖励的表达方式为 torch.max(self.model(next_state[idx]))
             target[idx][torch.argmax(action[idx]).item()] = Q_new
+            # 对于每一步，将当前动作的价值更新为Q_new
     
         # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
         # pred.clone()
         # preds[argmax(action)] = Q_new
+        # 计算target和pred之间的损失值，得到loss
         self.optimizer.zero_grad()
         loss = self.criterion(target, pred)
         loss.backward()
